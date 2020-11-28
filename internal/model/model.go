51a9fd319c4f400a24e9df3e5c81f23e073f7b17
Andre930 <969347529@qq.com>
2020-11-28 15:48:16 +0800

    Add gorm v2  common callback function

--- internal/model/model.go
+++ internal/model/model.go
@@ -6,9 +6,11 @@ import (
 	"github.com/go-programming-tour-book/blog-service/pkg/setting"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 	"log"
+	"time"
 )
 
 type Model struct {
@@ -44,6 +46,10 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 		DontSupportRenameColumn:   true,    // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
 		SkipInitializeWithVersion: false,   // 根据当前 MySQL 版本自动配置
 	}), gormConfig)
+	//替换 gorm  create update 流程中自带的回调函数
+	db.Callback().Create().Replace("gorm:before_create", updateTimeStampForBeforeCreateCallback)
+	db.Callback().Update().Replace("gorm:before_update", updateTimeStampForBeforeUpdateCallback)
+	db.Callback().Delete().Replace("gorm:delete",deleteCallback)
 	if err != nil {
 		log.Printf("connstr :%v  err: %+v \n", connStr, err.Error())
 		panic(err)
@@ -53,5 +59,62 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 		db.Logger.LogMode(logger.Info)
 	}
 	return db, nil
+
+}
+
+func updateTimeStampForBeforeCreateCallback(db *gorm.DB) {
+	db.Statement.SetColumn("CreatedOn", time.Now().Unix())
+}
+
+func updateTimeStampForBeforeUpdateCallback(db *gorm.DB) {
+	db.Statement.SetColumn("ModifiedOn", time.Now().Unix())
+
+}
+
+//   实现软删除
+func deleteCallback(db *gorm.DB) {
+	if db.Error == nil {
+		if db.Statement.Schema != nil {
+			db.Statement.SQL.Grow(100)
+			deleteField := db.Statement.Schema.LookUpField("DeletedOn")
+			isDelField := db.Statement.Schema.LookUpField("IsDel")
+			if !db.Statement.Unscoped && deleteField != nil && isDelField != nil {
+				//soft delete
+				if db.Statement.SQL.String() == "" {
+					nowTime := time.Now().Unix()
+					db.Statement.AddClause(
+						clause.Set{
+							{Column: clause.Column{Name: deleteField.DBName},
+								Value: nowTime},
+							{Column: clause.Column{Name: isDelField.DBName},
+								Value: 1},
+						})
+					db.Statement.AddClauseIfNotExists(clause.Update{})
+					db.Statement.Build("UPDATE", "SET", "WHERE")
+				}
+			} else {
+				//delete
+				if db.Statement.SQL.String() == "" {
+					db.Statement.AddClauseIfNotExists(clause.Delete{})
+					db.Statement.AddClauseIfNotExists(clause.From{})
+					db.Statement.Build("DELETE", "SET", "WHERE")
+				}
+			}
+			//must need where
+			if _, ok := db.Statement.Clauses["WHERE"]; !db.AllowGlobalUpdate && !ok {
+				db.AddError(gorm.ErrMissingWhereClause)
+				return
+			}
+			// exec sql
+			if !db.DryRun && db.Error == nil {
+				result, err := db.Statement.ConnPool.ExecContext(db.Statement.Context, db.Statement.SQL.String(), db.Statement.Vars...)
+				if err != nil {
+					db.RowsAffected, _ = result.RowsAffected()
+				} else {
+					db.AddError(err)
+				}
+			}
+		}
+	}
 
 }
