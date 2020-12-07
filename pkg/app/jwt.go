8ca73dd89879425b0ace11bc10dd34cc093fd7af
Andre930 <969347529@qq.com>
2020-12-07 22:47:18 +0800

    Finish Jwt config function

--- pkg/app/jwt.go
+++ pkg/app/jwt.go
@@ -1,9 +1,55 @@
 package app
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/pkg/util"
+	"time"
+)
 
 type Claims struct {
 	AppKey string 	`json:"app_key"`
 	AppSecret string `json:"app_secret"`
 	jwt.StandardClaims
+}
+
+// 获取JWT secret
+func GetJWTSecret() []byte {
+	return []byte(global.JWTSetting.Secret)
+
+}
+
+// 根据AppKey AppSecret 以及在项目配置中设置的签发者 （Isuuer） 和过期时间（ExpiresAt）,根据指定的算法生成签名后的Token.
+func GenerateToken(appKey, appSecret string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(global.JWTSetting.Expire)
+	claims :=Claims{
+		AppKey:         util.EncodeMD5(appKey),
+		AppSecret:      util.EncodeMD5(appSecret),
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer: global.JWTSetting.Issuer,
+		},
+	}
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(GetJWTSecret())
+	return token,err
+
+}
+
+// 主要功能是解析和校验token，其流程是解析传入的token,然后根据Cliams 的相关属性要求进行校验。
+func ParseToken(token string) (*Claims,error) {
+	tokenCliams, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return GetJWTSecret(), nil
+
+	})
+	if tokenCliams!=nil {
+		claims,ok := tokenCliams.Claims.(*Claims)
+		if ok && tokenCliams.Valid {
+			return claims,nil
+
+		}
+
+	}
+	return nil, err
 }
